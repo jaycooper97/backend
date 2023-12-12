e612df3abfce1ce97d572746cc3729eeaa3fa68c
jay cooper <15jacooper@gmail.com>
2023-12-11 20:14:13 -0500

    pushing mongoose

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 app.get('/', (req,res) =>{
     res.setHeader("Access-Control-Allow-Credentials", "true")
 })
-
+// comment
 
 //grabs all routes in router
 app.use('/attractions', cincy);
